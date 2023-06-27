309ae83bfce6cff4912140a2219b0327e039ed9a
Hasala <hasaladissanayake@yahoo.com>
2023-06-27 11:15:20 +0530

    handling clients

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 import socket
 import threading
 
+HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 # print(SERVER)
 ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
 #created a socket, picked a family AF_NET, picked the type SOCK_STEAM
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,8 +15,22 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
 #wait for clients
+#will run concurrently for each client
 def handle_client(conn, addr):
-    pass
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        #this wont execute untill a msg recieved form the client (blocking code)
+        #how many bytes we are going to take is inside recv() 
+        #to specify that we need a header (always so it should be 64 bytes)
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        #convert it to int
+        msg_length= int(msg_length)
+        #put that here as how many byte recieved
+        msg = conn.recv(msg_length).decode(FORMAT)
+        print(f"[{addr}] {msg}")
+
 
 #start the socket server
 #allow connections and handling them
