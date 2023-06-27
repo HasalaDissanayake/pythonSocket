5253b190b852b843a7f7da49ad07dccde70dbc29
Hasala <hasaladissanayake@yahoo.com>
2023-06-27 11:47:49 +0530

    waiting for keyboard inputs

--- client.py
+++ client.py
@@ -9,3 +9,23 @@ ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
+
+#to send msgs
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    #need to find the length of this message
+    #'b' means byte space
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+send("HEllow World")
+#can hold untill the keypress
+input()
+send("HEllow guys")
+input()
+send("HEllow john")
+
+send(DISCONNECT_MESSAGE)
